2026426ad8458d88e3c6fc42231f1cd939be2367
ozantoteles <ozantoteles@gmail.com>
2023-04-30 18:42:32 +0100

    add decoding
    This code adds a new route /decode to the Flask app that allows users to upload a QR code image. The uploaded image is then decoded using the pyzbar library and the decoded text is returned as the response.
    
    To use this updated version of the app, make sure you have the pyzbar library installed by running the command pip install pyzbar. Then, save the above code to a file named app.py and run it using the command python app.py. The updated app should now be running on your system and accessible at http://<serveraddress>:5000.

--- qrservice.py
+++ qrservice.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, send_file
+from pyzbar.pyzbar import decode
+from PIL import Image
 import qrcode
 from io import BytesIO
 
@@ -7,9 +9,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '''
+        <h3>Generate QR Code</h3>
         <form action="/generate" method="post">
             <input type="text" name="link" placeholder="Enter link">
             <input type="submit" value="Generate QR Code">
+        </form>
+        <br>
+        <h3>Decode QR Code</h3>
+        <form action="/decode" method="post" enctype="multipart/form-data">
+            <input type="file" name="qr_code">
+            <input type="submit" value="Decode QR Code">
         </form>
     '''
 
@@ -21,6 +30,16 @@ def generate():
     img.save(img_io, 'PNG')
     img_io.seek(0)
     return send_file(img_io, mimetype='image/png')
+
+@app.route('/decode', methods=['POST'])
+def decode_qr():
+    qr_code = request.files['qr_code']
+    img = Image.open(qr_code)
+    data = decode(img)
+    if data:
+        return data[0].data.decode('utf-8')
+    else:
+        return 'No QR code detected'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
