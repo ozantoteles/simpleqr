e4bb215d545b32f379225a74d83f14156a101ad6
ozantoteles <ozantoteles@gmail.com>
2023-04-30 18:41:28 +0100

    Upgrade to Flask service.
    This code creates a simple Flask app with two routes: / and /generate. The / route displays a form where the user can enter a link and submit it to generate a QR code. The /generate route takes the submitted link and uses the qrcode library to generate a QR code, which is then returned as an image.
    
    To run this app on your system, make sure you have Flask and the qrcode library installed by running the commands pip install Flask and pip install qrcode. Then, save the above code to a file named app.py and run it using the command python app.py. The app should now be running on your Raspberry Pi and accessible at http://serveraddress:5000.

--- qrservice.py
+++ qrservice.py
@@ -1,4 +1,26 @@
+from flask import Flask, request, send_file
 import qrcode
-data = 'https://www.example.com'
-img = qrcode.make(data)
-img.save('MyQRCode.png')
+from io import BytesIO
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '''
+        <form action="/generate" method="post">
+            <input type="text" name="link" placeholder="Enter link">
+            <input type="submit" value="Generate QR Code">
+        </form>
+    '''
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    link = request.form['link']
+    img = qrcode.make(link)
+    img_io = BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
